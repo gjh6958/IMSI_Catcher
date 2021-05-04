fdc74cee6114a3ed67de5b666f7128c6ffb0e558
gray <gjh6958@rit.edu>
2021-05-04 19:18:52 -0400

    MVP of project.  Finished generating correct configuration files.  Finished MVP code.  Finished IMSI.py functionality. TODO: Add config generation python file from Joe.

--- IMSI.py
+++ IMSI.py
@@ -2,11 +2,12 @@ import os
 import subprocess
 import time
 import os.path
+import signal
 
 # Initializes the variables to be used by the rest of the program
 # This is where changes in directorys, output files, etc. should go
 def init():
-    prefix = "/home/gray/.config/srslte/gen/"
+    prefix = "/home/gray/.config/srslte/POC/"
     conf = ".conf"
     carriers = ["ATT", "TMobile", "Verizon"]
     MCC = ["310", "311"]
@@ -33,26 +34,30 @@ def runConfigs(prefix, conf, carriers, MCC, outputFile, pwd):
                 while( os.path.isfile(epcFile) ):
                     enbNum = 0 # Keeps track of the number enb configuration file to be used under the epc
                     print("\tLoading epc configuration: %s" % epcFile)
-                    epcargs = ["sudo", "srsepc", epcFile]
+                    epcargs = ["srsepc", epcFile]
+                    enbFile = prefix + carrier + "/" + carrier + "enb_" + i + "_" + str(epcNum) + "_" + str(enbNum) + conf
                     epc = subprocess.Popen(epcargs, stdout=output)
                     print("\tRunning epc...")
-                    enbFile = prefix + carrier + "/" + carrier + "enb_" + i + "_" + str(epcNum) + "_" + str(enbNum) + conf
                     # Loop through all enb files coorelating to the epc file
                     while( os.path.isfile(enbFile) ):
                         print("\t\tLoading enb configuration: %s" % enbFile)
-                        enbargs = ["sudo", "srsenb", enbFile]
-                        enb = subprocess.Popen(enbargs, stdout=output)
+                        enbargs = ["srsenb", enbFile]
+                        enb = subprocess.Popen(enbargs, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
                         print("\t\tRunning enb...")
-                        time.sleep(2) # Run for 15s
+                        time.sleep(30) # Run for 15s
                         print("\t\tTerminating enb...")
-                        enb.terminate() # Terminate the enb backround process
+                        os.kill(enb.pid, signal.SIGTERM) # Terminate the enb backround process
+                        time.sleep(5) # Allow time for graceful enb shutdown
                         enbNum += 1
                         enbFile = prefix + carrier + "/" + carrier + "enb_" + i + "_" + str(epcNum) + "_" + str(enbNum) + conf
                     print("\tTerminating epc...")
-                    epc.terminate() # Terminate the epc backgroud process
+                    os.kill(epc.pid, signal.SIGTERM) # Terminate the epc backgroud process
+                    time.sleep(5) # Allow time for graceful epc shutdown
                     epcNum += 1
                     epcFile = epcCarrierPrefix + i + "_" + str(epcNum) + conf
 
+# Finds IMSIs contained within the output file and returns them as a set
+# preventing multiple enumerations of the same IMSI
 def findIMSI(outputFile):
     IMSI = set()
     with open(outputFile, 'r') as output:
@@ -60,8 +65,12 @@ def findIMSI(outputFile):
         for line in content:
             line = line.split(" ")
             if "IMSI:" in line:
-                index = [x for x in range(len(line)) if line[x] == "IMSI:"] + 1
+                index = 0
+                for i in range(len(line)):
+                    if(line[i] == "IMSI:"):
+                        index = i+1
                 IMSI.add(line[index])
+    return IMSI
 
 
 
@@ -69,6 +78,8 @@ def findIMSI(outputFile):
 initConf = init()
 runConfigs(initConf[0], initConf[1], initConf[2], initConf[3], initConf[4], initConf[5])
 IMSI = findIMSI(initConf[4])
+
+# Print all IMSIs found
 if len(IMSI) == 0:
     print("No IMSIs found...")
 else:
